225edaca52357e6ca581f71441fffd6aa50c4a1e
Yimizhao <zhao12051610@163.com>
2018-03-24 14:06:21 +0800

    更新

--- src/main/java/com/smart/web/LoginController.java
+++ src/main/java/com/smart/web/LoginController.java
@@ -6,16 +6,14 @@ import javax.servlet.http.HttpServletRequest;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.smart.domain.User;
 import com.smart.service.UserService;
 
-@Controller
-//@RestController
+@RestController
 public class LoginController{
 	private UserService userService;
     
